0dbdc888a08a5b56611b3da0f5f3b9c476638ec1
SooHwanCho <chh0107@gmail.com>
2019-12-12 15:48:31 +0900

    Initial Commit

---
+++ CodilityAlgorithm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CodilityAlgorithm
+//
+//  Created by 조수환 on 2019/12/12.
+//  Copyright © 2019 조수환. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
